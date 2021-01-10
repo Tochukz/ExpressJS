6ab5372e148caf58f0c2c71c250dd1f9d3148f8a
Tochukwu Nwachukwu <truetochukz@gmail.com>
2021-01-10 21:57:02 +0200

    API Token Authentication

--- auth/api-auth/data/users.js
+++ auth/api-auth/data/users.js
@@ -2,11 +2,13 @@ module.exports = [
     {
         name: 'John Smith',
         username: 'johnny',
-        token: 'N4fmqEFbwFVG8CNYFELSqnnmQGWnRd78TxRSRje7SCrCS8ZMXi5rsyabc2a4'
-    },
+        password: 'johnny@pass',
+        token: 'dea10a8b873af1baa868469ca7a74a06964ab6861c4da65b5b054f87662dd9f1d804855e1bab67790f9720e9612644e558f2afba76abc21ef38bf91eacdb14dd'
+,    },
     {
         name: 'James Young',
         username: 'youngy',
-        token: 'flXUYvLrDwCzfCpAJG6ldP21N7oRe1Ej68IAaTVK09UzQiIDxSqI3jziuOFZ'
+        password: 'youngy@pass',
+        token: 'a68d0e45a5d1b1fe27c06436ac1c303851d00ac182f69ebf7d48ca68dca1b799282df65783437ec1f345003f4d4dc1f4ad29582e2f5bdad2204cb85695630c48'
     }
 ];
--- auth/api-auth/routes/auth.js
+++ auth/api-auth/routes/auth.js
@@ -6,11 +6,13 @@ const users = require('../data/users');
 router.post('/login', (req, res, next) => {
     let {username, password} = req.body;
     username = username || "empty";
-    const user = users.find(usr => usr.username.toLowerCase() == username.toLowerCase());
+    const user = users.find(usr => usr.username.toLowerCase() == username.toLowerCase() && password == usr.password);
     if (!user) {
         return res.status(401).send({message: 'Invalid username and/or password'});   
     }
-    return res.send({message: 'Login Successful'});
+    return res.send({
+        token: user.token,
+    });
 });
 
 router.post('/logout',  (req, res, next) => {
